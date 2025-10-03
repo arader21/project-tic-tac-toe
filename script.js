ce5ff2b09077b35d864777d7817f7367b72b7619
Austin Rader <austinrader21@gmail.com>
2025-10-03 10:57:15 -0600

    change player function added and check winner

--- script.js
+++ script.js
@@ -30,3 +30,38 @@ updateCell(this, cellIndex);
 changePlayer();
 checkWinner();
 }
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+function checkWinner(){
+let roundWon = false;
+for(let i = 0; i < winConditions.length; i++){
+    const condition = winConditions[i];
+    const cellA = options[condition[1]];
+    const cellB = options[condition[2]];
+    const cellC = options[condition[3]];
+if(cellA == "" || cellB == "" || cellC == ""){
+    continue;
+    }
+if(cellA == cellB && cellB == cellC){
+    roundWon = true;
+    break;
+    }
+}
+if(roundWon){
+    statusText.textContent = `${currentPlayer} wins!`;
+    running = false;
+}
+else if (options.includes("")){
+statusText.textContent = `Draw!`;
+running = false;
+}
+}
+function restartGame(){
+
+}
